7a789faa863d9828f90347945a62ae64d760fa7b
Thilo <tfr@e-rat.de>
2019-11-14 19:49:40 +0100

    Merge + weitere prefabs

---
+++ Feld.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Feld : MonoBehaviour
+{
+    public FeldInhalt inhalt;
+
+    /// <summary>
+    /// Methode zum Zuweisen eines Feldinhaltes
+    /// Inhalt instanziieren und im Anschluss zuweisen
+    /// </summary>
+    /// <param name="perfab">das zu initialiesierende prefab</param>
+    public void initInhalt(GameObject perfab)
+    {
+        GameObject temp = Instantiate<GameObject>(perfab);
+        temp.transform.parent = this.transform; //Bewegungen synchronisieren
+        temp.transform.localPosition = Vector3.zero; //Position im Feld an richtige Stelle
+        temp.transform.localRotation = Quaternion.identity; //Rotierung ausrichten
+        inhalt = temp.GetComponent<FeldInhalt>();
+    }
+}
